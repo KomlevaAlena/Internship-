50a0c40aa0eee825674eb46dec6e13f2e7785b69
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2024-09-23 07:01:07 +0500

    html

--- source/js/main.js
+++ source/js/main.js
@@ -5,7 +5,7 @@
 
 import {iosVhFix} from './utils/ios-vh-fix';
 import {Form} from './modules/form-validate/form';
-// import {sliderHero} from './modules/slider';
+import {sliderHero} from './modules/slider';
 // import {sliderTours} from './modules/slider';
 // import {sliderTraining} from './modules/slider';
 // import {sliderReviews} from './modules/slider';
@@ -22,7 +22,7 @@ window.addEventListener('DOMContentLoaded', () => {
     window.form = form;
     form.init();
     // mobileMenu();
-    // sliderHero.init();
+    sliderHero.init();
     // sliderTours.init();
     // sliderTraining.init();
     // sliderReviews.init();
@@ -32,3 +32,21 @@ window.addEventListener('DOMContentLoaded', () => {
     // window.addEventListener('resize', sliderGallery);
   });
 });
+
+
+// <!-- пример использования спрайта >
+//     <img src="/__spritemap#sprite-logo-view" alt="Это спрайт">
+//     <svg>
+//       <use href="/__spritemap#sprite-user"></use>
+//     </svg>
+//     <!-- примеры требуемых атрибутов data-test="*" >
+//     <div data-test="hero">hero</div>
+//     <div data-test="about">about</div>
+//     <div data-test="programs">programs</div>
+//     <div data-test="grant">grant</div>
+//     <div data-test="news">news</div>
+//     <div data-test="FAQ">FAQ</div>
+//     <div data-test="reviews">reviews</div>
+//     <div data-test="contacts">contacts</div>
+//     <div data-test="form">form</div>
+//     <div-- data-test="footer">footer</div-->
--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,45 +1,45 @@
-// import Swiper from 'swiper';
-// import {Autoplay, Navigation, Pagination} from 'swiper/modules';
-// import 'swiper/css';
+import Swiper from 'swiper';
+import {Autoplay, Navigation, Pagination} from 'swiper/modules';
+import 'swiper/css';
 
-// export const sliderHero = new Swiper('.hero__wrapper', {
-//   direction: 'horizontal',
-//   loop: true,
-//   autoplay: {
-//     delay: 3000,
-//     disableOnInteraction: false,
-//   },
-//   pagination: {
-//     el: '.hero-pagination',
-//     clickable: true,
-//   },
-//   modules: [Autoplay, Navigation, Pagination],
-//   breakpoints: {
-//     320: {
-//       slidesPerView: 1,
-//       spaceBetween: 0,
-//     },
-//     768: {
-//       slidesPerView: 1,
-//       spaceBetween: 0,
-//     },
-//     1440: {
-//       slidesPerView: 1,
-//       spaceBetween: 0,
-//     },
-//   },
-//   on: {
-//     init() {
-//       this.el.addEventListener('mouseenter', () => {
-//         this.autoplay.stop();
-//       });
+export const sliderHero = new Swiper('.hero__wrapper', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.hero-pagination',
+    clickable: true,
+  },
+  modules: [Autoplay, Navigation, Pagination],
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    1440: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+  },
+  on: {
+    init() {
+      this.el.addEventListener('mouseenter', () => {
+        this.autoplay.stop();
+      });
 
-//       this.el.addEventListener('mouseleave', () => {
-//         this.autoplay.start();
-//       });
-//     }
-//   },
-// });
+      this.el.addEventListener('mouseleave', () => {
+        this.autoplay.start();
+      });
+    }
+  },
+});
 
 // export const sliderTours = new Swiper('.tours__wrapper', {
 //   direction: 'horizontal',
